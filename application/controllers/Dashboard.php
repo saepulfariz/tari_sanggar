e5d271a0d7cd0a084ceb41a6eab7880212f983b7
saepulfariz <saepulfariz108@gmail.com>
2023-11-21 17:19:11 +0700

    add content dashboard view

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -9,11 +9,19 @@ class Dashboard extends CI_Controller
   {
     parent::__construct();
     cekLogin();
+    $this->load->model('SanggarOrderModel', 'order');
+    $this->load->model('SanggarPaketModel', 'paket');
+    $this->load->model('SanggarModel', 'sanggar');
+    $this->load->model('UserModel', 'user');
   }
 
   public function index()
   {
     $data['title'] = 'Dashboard';
+    $data['order_new'] = $this->order->select("COUNT(id) as jumlah")->where('status', 'PENDING')->first()['jumlah'];
+    $data['order'] = $this->order->select("COUNT(id) as jumlah")->where('status !=', 'REJECT')->where('status !=', 'PENDING')->first()['jumlah'];
+    $data['konsumen'] = $this->user->select("COUNT(id) as jumlah")->where('id_role', 3)->first()['jumlah'];
+    $data['sanggar'] = $this->user->select("COUNT(id) as jumlah")->first()['jumlah'];
     $this->template->load('template/index', 'dashboard/index', $data);
   }
 }
--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -23,16 +23,31 @@
     <div class="row">
       <div class="col-lg-3 col-6">
         <!-- small box -->
+        <div class="small-box bg-danger">
+          <div class="inner">
+            <h3><?= $order_new; ?></h3>
+
+            <p>New Order</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-pie-graph"></i>
+          </div>
+          <a href="<?= base_url('order'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
         <div class="small-box bg-info">
           <div class="inner">
-            <h3>150</h3>
+            <h3><?= $order; ?></h3>
 
-            <p>New Orders</p>
+            <p>Total Order</p>
           </div>
           <div class="icon">
             <i class="ion ion-bag"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="<?= base_url('order'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <!-- ./col -->
@@ -40,9 +55,9 @@
         <!-- small box -->
         <div class="small-box bg-success">
           <div class="inner">
-            <h3>53<sup style="font-size: 20px">%</sup></h3>
+            <h3><?= $konsumen; ?></h3>
 
-            <p>Bounce Rate</p>
+            <p>Konsumen</p>
           </div>
           <div class="icon">
             <i class="ion ion-stats-bars"></i>
@@ -55,32 +70,18 @@
         <!-- small box -->
         <div class="small-box bg-warning">
           <div class="inner">
-            <h3>44</h3>
+            <h3><?= $sanggar; ?></h3>
 
-            <p>User Registrations</p>
+            <p>Sanggar</p>
           </div>
           <div class="icon">
             <i class="ion ion-person-add"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="<?= base_url('sanggar'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <!-- ./col -->
-      <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-danger">
-          <div class="inner">
-            <h3>65</h3>
 
-            <p>Unique Visitors</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-pie-graph"></i>
-          </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
     </div>
     <!-- /.row -->
 
